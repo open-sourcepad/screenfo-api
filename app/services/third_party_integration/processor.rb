d03d30cf2b8d898fdc5069336f23bfa71b5241ae
Karl Potenciano <karlp@sourcepad.com>
2017-03-11 00:23:13 +0800

    log token

--- app/services/third_party_integration/processor.rb
+++ app/services/third_party_integration/processor.rb
@@ -18,6 +18,7 @@ module ThirdPartyIntegration
       params = {"email"=> @params["username"], "password"=> @params["password"]}
       response = HttpService::Base.new(N24_LOGIN).post(params)
       if response["status"] == "200" || response["status"] == 200
+        puts response["body"]["token"]
         @user.update_attributes(n24_access_token: response["body"]["token"])
       end
     end
